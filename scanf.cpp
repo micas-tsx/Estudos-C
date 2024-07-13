2657207a080a299b78c4c8a2600d807db69b8e29
Micael Abud <84108378+micas-tsx@users.noreply.github.com>
2024-07-13 12:47:08 -0300

    Add files via upload

---
+++ scanf.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int idade = 0;
+	
+	printf("valor inicial da idade: %d \n", idade);
+	
+	printf("digite uma idade: \n");
+	scanf("%d", &idade);
+	
+	printf("idade informada: %d \n", idade);
+}
