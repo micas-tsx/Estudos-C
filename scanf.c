3656b0d7b66a3ff2d9cb5e0c06416bfc637155d8
Micael Abud <84108378+micas-tsx@users.noreply.github.com>
2024-07-13 13:55:44 -0300

    idade e peso

--- scanf.c
+++ scanf.c
@@ -3,10 +3,14 @@
 int main() {
     
     int idade = 0;
+    float peso = 0.0;
     
     printf("Digite a idade: \n");
     scanf("%d", &idade);
 
-    printf("sua idade é: %d\n", idade);
+    printf("Digite o seu peso: \n");
+    scanf("%f", &peso);
+
+    printf("sua idade e seu peso é: %d e %f \n", idade, peso);
 
 }
